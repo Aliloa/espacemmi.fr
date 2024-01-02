7bbc5abd8df882dea9bf9c09863b9ef845b06766
offkrn <124247628+Krxrn@users.noreply.github.com>
2024-01-02 21:08:33 +0100

    bdd

--- ENT.sql
+++ ENT.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 27 déc. 2023 à 10:36
+-- Généré le : mar. 02 jan. 2024 à 20:05
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `ent`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+DROP TABLE IF EXISTS `cours`;
+CREATE TABLE IF NOT EXISTS `cours` (
+  `id_cours` int NOT NULL AUTO_INCREMENT,
+  `cours` varchar(50) NOT NULL,
+  `prof` varchar(50) NOT NULL,
+  `img` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_cours`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `cours`
+--
+
+INSERT INTO `cours` (`id_cours`, `cours`, `prof`, `img`) VALUES
+(1, 'Protocole SAE 3.02', 'Renault Einpstein', 'img/1-dev.png'),
+(2, 'Marketing POST 8890', 'Leyla Jaoued', 'img/1-comp.png'),
+(3, 'Protocole Narratif Bande Son', 'Karim Pierrre Chabane', 'img/1-conc'),
+(4, 'Display Grid', 'Gaelle Charpentier', 'img/1-dev.png'),
+(5, 'Consigne production sonore', 'Tony Houziaux', 'img/1-conc');
 
 -- --------------------------------------------------------
 
@@ -48,6 +74,34 @@ INSERT INTO `crous` (`id`, `entre`, `plat`, `dessert`, `date`, `image_plat`) VAL
 (6, 'Soupe de potiron, Ceviche de poisson', 'Poulet curry coco, Filet mignon aux champignons, Lasagnes végétariennes', 'Tarte aux pommes, Mousse au chocolat, Panna cotta aux fruits rouges, Fondue au chocolat avec fruits ', '2023-12-26', 'https://i.pinimg.com/564x/a5/81/56/a58156cbd213bb58cdc4d24160c6b2c9.jpg'),
 (7, 'Carpaccio de saumon aux agrumes, Salade de chèvre chaud au miel', 'Filet de boeuf, Risotto aux champignons, Poulet rôti', 'Tiramisu aux fruits rouges, Crème brûlée à la vanille, Fondant au chocolat', '2023-12-30', 'https://img.cuisineaz.com/660x660/2017/09/04/i132139-risotto-aux-champignons-au-companion.jpeg'),
 (8, 'Velouté de potiron à la crème fraîche, Bruschetta à la tomate et mozzarella', 'Pâtes carbonara, Magret de canard à l\'orange, Poisson en croûte d\'amandes', 'Mousse au chocolat noir, Tarte aux pommes caramélisées, Sorbet au citron', '2023-12-31', 'https://img.cuisineaz.com/1024x1024/2015/10/12/i6388-magret-de-canard-a-l-orange-au-miel.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `login` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_utilisateurs` int NOT NULL AUTO_INCREMENT,
+  `mot_de_passe` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_utilisateurs`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`login`, `id_utilisateurs`, `mot_de_passe`) VALUES
+('loana.chalach', 1, '$2y$10$1mhX1X9WpFmpqnfmVAYYc.NEUGAOl6fD2T/57w16GIqrWUCVKzSQO'),
+('loana.chalach', 2, '$2y$10$KEtXbV1eHYYKraJo39HeouPDGxRB/I9L0SoecYxFPqJO1d783QXx.'),
+('lolo', 3, '$2y$10$nQB2aG.qbFowrU.A1qbqbOo1J.UCWR3pmAvjh9U00UhUZ4hO8sV2u'),
+('lolo', 4, '$2y$10$dILRNr2b86WMr3nwo/xMf.CDiuWpdrnrSFPzpP50VRihKR1U60VRO'),
+('loanacha', 5, '$2y$10$3xWP6KntZaUWICjDoZ1yAuIgQDmsyUoVtbn3K9uDgHWjTTNYPW2B.'),
+('alinaspicot@gmail.com', 6, '$2y$10$/q8TvQre1/VFWB5FQhkF1ObMYilYUkLezOPylxSHEDMZhM2r4Hg3W'),
+('alinaspicot@gmail.com', 7, '$2y$10$b2oJw34seok5jQRhf7ylP.CEV8DkXYLAg.xDgkjTJKrDtwNhVPUHe'),
+('alinaspicot@gmail.com', 8, '$2y$10$HQqF3ed/.ZH8EdyAs7YqaO3gEDdHm11n9DQScO9ywY7CIJQf18VWe');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
