f8e64a4a25b24a522f061453112d82bfd8c109b7
Anchana <fatimarajananchana@gmail.com>
2024-01-10 19:35:18 +0100

    Update vie_scolaire.php

--- vie_scolaire.php
+++ vie_scolaire.php
@@ -228,14 +228,15 @@
         </div>
 
         <?php
-      $requete = "SELECT abscence_retard.id_abs, abscence_retard.titre, abscence_retard.date, abscence_retard.nombre, cours.cours
-      FROM abscence_retard, cours
-      WHERE abscence_retard.cours = cours.id_cours
-      ORDER BY abscence_retard.id_abs DESC";
-
+        // $requete = "SELECT * FROM abscence_retard, cours WHERE ext_cours = cours ORDER BY id_abs DESC";
+        $requete = "SELECT abscence_retard.id_abs, abscence_retard.titre, abscence_retard.date, abscence_retard.nombre, cours.cours
+            FROM abscence_retard, cours
+            WHERE abscence_retard.ext_cours = cours.id_cours
+            ORDER BY abscence_retard.id_abs DESC";
         $stmt = $db->query($requete);
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
+
         <div class='bloc-3a'>
             <div class="bouton">
                 <h1>Absences et retards</h1>
@@ -259,6 +260,7 @@
                 <?php } ?>
             </div>
         </div>
+
 
 
 
