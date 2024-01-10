01f1d7771cc19ab740a351e2f38b5fff5af3f342
Loaxn <chalachloana@gmail.com>
2024-01-10 23:19:21 +0100

    opquast modification

--- mail_message.php
+++ mail_message.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
