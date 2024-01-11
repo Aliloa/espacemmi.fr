bca5e828da617c3418b2234d4420408b907f4252
Anchana <fatimarajananchana@gmail.com>
2024-01-11 19:26:31 +0100

    changement

--- messagerie.php
+++ messagerie.php
@@ -13,15 +13,11 @@
         header('Location: index.php?access_denied');
         exit();
     }
-    if (isset($_SESSION["role"]) && $_SESSION["role"] === 'Enseignant.e') {
-        header('Location: backofficeprof.php?access_denied');
-    }
+
     if (isset($_SESSION["role"]) && $_SESSION["role"] === 'Membre du CROUS') {
         header('Location: page_crous.php?access_denied');
     }
-    if (isset($_SESSION["role"]) && $_SESSION["login"] === 'Admin') {
-        header('Location: administration.php?access_denied');
-    }
+
 
     ?>
     <header>
