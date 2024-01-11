98b31189c2f91418e351dcf0f7da3b4a0a890305
Anchana <fatimarajananchana@gmail.com>
2024-01-11 15:55:36 +0100

    sécurité

--- accueil.php
+++ accueil.php
@@ -22,6 +22,9 @@
     if (isset($_SESSION["role"]) && $_SESSION["role"] === 'Enseignant.e') {
         header('Location: backofficeprof.php?access_denied');
     }
+    if (isset($_SESSION["role"]) && $_SESSION["role"] === 'Membre du CROUS') {
+        header('Location: backofficeprof.php?access_denied');
+    }
     if (isset($_SESSION["role"]) && $_SESSION["login"] === 'Admin') {
         header('Location: administration.php?access_denied');
     }
