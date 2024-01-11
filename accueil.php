3590596a8f518f1adba98f1099cbbb84815dd8f7
Anchana <fatimarajananchana@gmail.com>
2024-01-11 15:53:56 +0100

    conditions

--- accueil.php
+++ accueil.php
@@ -21,9 +21,11 @@
     }
     if (isset($_SESSION["role"]) && $_SESSION["role"] === 'Enseignant.e') {
         header('Location: backofficeprof.php?access_denied');
-
-
     }
+    if (isset($_SESSION["role"]) && $_SESSION["login"] === 'Admin') {
+        header('Location: administration.php?access_denied');
+    }
+
     ?>
     <header>
         <div class='menu'>
