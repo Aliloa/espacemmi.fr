d032f60b7980be48dc8a5e6654bb06f27a978412
Loaxn <chalachloana@gmail.com>
2024-01-11 13:29:58 +0100

    Update formulaire_mdp.php

--- formulaire_mdp.php
+++ formulaire_mdp.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -17,75 +14,66 @@ session_start();
     <style>
         .message-erreur {
             color: red;
-            margin:0;
+            margin: 0;
         }
     </style>
 </head>
 
 <body>
-    <div class="main">
-        <div class="logo">
-            <a href="index.php"><img src="img/logo(2).png"></a>
+    <main class="main-2">
+
+        <div class="logo-1">
+            <a href="index.php"><img src="img/logo.svg" alt="page d'accueil de l'ent"></a>
+            <a href="index.php"><img src="img/logo-long.png" alt="page d'accueil de l'ent"></a>
         </div>
 
         <div class="formulaire">
             <form action="formulaire_mdp_traite.php" method="POST" onsubmit="return validateForm()">
 
-                <h1>Mot de passe </h1>
-                 <label for="login">Login:</label>
-                <input type="text" id="login" name="login_user" required>
-                <label for="mdp1">Entrez votre nouveau mot de passe:</label>
-                <input type="password" id="mdp1" name="mdp1" required>
-                <label for="mdp2">Répétez le mot de passe: </label>
-                <input type="password" id="mdp2" name="mdp2" required>
+                <h1>Mot de passe</h1>
+
+                <label for="login">Login:</label><br>
+                <input class="bouton2" type="text" id="login" name="login_user" required><br>
+
+                <label for="mdp1">Entrez votre nouveau mot de passe:</label><br>
+                <input class="bouton2" type="password" id="mdp1" name="mdp1" required>
+
+                <label for="mdp2">Répétez le mot de passe: </label><br>
+                <input class="bouton2" type="password" id="mdp2" name="mdp2" required>
 
                 <div class="message-erreur" id="messageErreur"></div>
 
                 <div class="bouton1">
                     <input type='submit' value='Réinitialiser'>
                 </div>
+
             </form>
 
-            <script>
-                function validateForm() {
-                    var mdp1 = document.getElementById("mdp1").value;
-                    var mdp2 = document.getElementById("mdp2").value;
-                    var messageErreur = document.getElementById("messageErreur");
+            <div class="image2">
+                <img src="img/main_image.svg">
+            </div>
+        </div>
+    </main>
 
-                    if (mdp1 !== mdp2) {
-                        messageErreur.innerHTML = "Les mots de passe ne correspondent pas.";
-                        return false;
-                    } else {
-                        messageErreur.innerHTML = "";
-                        return true;
-                    }
-                }
-            </script>
-        </div>
-        <div class="image2">
-            <img src="img/main_image.png">
-        </div>
-    </div>
     <footer>
-        <div class="mentions-legales" onclick="openModal()">Mentions Légales</div>
+        <a href='mentions_legales.html'>
+            <p> Mentions légales </p>
+        </a>
     </footer>
 
-    <!-- Modal -->
-    <div id="myModal" class="modal" onclick="closeModal()">
-        <div class="modal-content" onclick="event.stopPropagation();">
-            <span class="close" onclick="closeModal()">&times;</span>
-            <h2>Mentions Légales</h2>
-            <p>Ajoutez ici le contenu de vos mentions légales.</p>
-        </div>
-    </div>
-
     <script>
-        function openModal() {
-            document.getElementById('myModal').style.display = 'flex';
-        }
+        function validateForm() {
+            var mdp1 = document.getElementById("mdp1").value;
+            var mdp2 = document.getElementById("mdp2").value;
+            var messageErreur = document.getElementById("messageErreur");
 
-        function closeModal() {
-            document.getElementById('myModal').style.display = 'none';
+            if (mdp1 !== mdp2) {
+                messageErreur.innerHTML = "Le deuxième mot de passe ne correspond pas au premier.";
+                return false;
+            } else {
+                messageErreur.innerHTML = "";
+                return true;
+            }
         }
     </script>
 </body>
